0ed0bbc876c79d237359d54fca83819773097f5f
Yuvraj Chopra <101337798+YuvrajxChopra@users.noreply.github.com>
2022-05-05 20:55:35 +0530

    build files
    added build files

---
+++ build/precache-manifest.5df66c0a02d9dc3b39368a03e88e55e6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a04d5d501b2702c00b6694d4a5681db",
+    "url": "/iPod/index.html"
+  },
+  {
+    "revision": "099ac6f2b5410380bda3",
+    "url": "/iPod/static/css/main.a2e7bd60.chunk.css"
+  },
+  {
+    "revision": "623d2a99ddffa95676e9",
+    "url": "/iPod/static/js/2.4667406a.chunk.js"
+  },
+  {
+    "revision": "d8341c57f580e145d476bfdbdb6453b4",
+    "url": "/iPod/static/js/2.4667406a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099ac6f2b5410380bda3",
+    "url": "/iPod/static/js/main.c836cb0e.chunk.js"
+  },
+  {
+    "revision": "9f2800e2662a2f09272c",
+    "url": "/iPod/static/js/runtime-main.3a22bdbc.js"
+  },
+  {
+    "revision": "971e3181ae6735636e2c49cca0418f61",
+    "url": "/iPod/static/media/John Denver - Country Roads.971e3181.jpg"
+  },
+  {
+    "revision": "a9f11dd7dc47db0768a140b89d89de83",
+    "url": "/iPod/static/media/John Denver - Country Roads.a9f11dd7.mp3"
+  },
+  {
+    "revision": "2640f833069973cd0823c1f709b6353a",
+    "url": "/iPod/static/media/Khalid - Young Dumb Broke.2640f833.jpg"
+  },
+  {
+    "revision": "e92d0309fbda5f2e2a063e0cc5834124",
+    "url": "/iPod/static/media/Khalid - Young Dumb Broke.e92d0309.mp3"
+  },
+  {
+    "revision": "a085f2f0164d605a717084c32baf2274",
+    "url": "/iPod/static/media/Never Gonna Give You Up.a085f2f0.png"
+  },
+  {
+    "revision": "4977e46cbdad85bad906db660d0a25a3",
+    "url": "/iPod/static/media/Post Malone - White Iverson.4977e46c.mp3"
+  },
+  {
+    "revision": "68d6ebbb90f987df3665106e55131836",
+    "url": "/iPod/static/media/Post Malone - White Iverson.68d6ebbb.png"
+  },
+  {
+    "revision": "ce3d38a985854c4640fe89ab293679ac",
+    "url": "/iPod/static/media/Rick Astley - Never Gonna Give You Up.ce3d38a9.mp3"
+  },
+  {
+    "revision": "70ab2f27726c9ce5f3eeb1b54787a478",
+    "url": "/iPod/static/media/Sigrid - High Five.70ab2f27.mp3"
+  },
+  {
+    "revision": "92a83443ea257110bbbedbaa7ac80d50",
+    "url": "/iPod/static/media/Sigrid - High Five.92a83443.png"
+  },
+  {
+    "revision": "0f702e6d9615af4ea6e4503fa3754645",
+    "url": "/iPod/static/media/game.0f702e6d.jpg"
+  },
+  {
+    "revision": "86f652825909378dd28f81eb87ed4dd3",
+    "url": "/iPod/static/media/music.86f65282.jpg"
+  },
+  {
+    "revision": "626bd78a090cd747f7bfe1addd07babd",
+    "url": "/iPod/static/media/settings.626bd78a.png"
+  },
+  {
+    "revision": "227f0ba9833ea10638eb851b4937d9f3",
+    "url": "/iPod/static/media/wallpaper1.227f0ba9.jpg"
+  },
+  {
+    "revision": "23077a93618a97f5d7254432926d3d69",
+    "url": "/iPod/static/media/wallpaper2.23077a93.jpg"
+  },
+  {
+    "revision": "b2fda5cab36fb65eccf326028ec5e1bb",
+    "url": "/iPod/static/media/wallpaper3.b2fda5ca.jpg"
+  }
+]);
